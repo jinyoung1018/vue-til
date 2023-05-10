564f5abc085b91a8bbd308ce66f2cb9ee1bd759f
jinyoung1018 <jywsb1816@gmail.com>
2023-05-10 14:50:12 +0900

    vue-til

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
+    <p> 
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
@@ -103,7 +103,7 @@ export default {
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- Add "scoped" attribute to limit CSS to this componeneet only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
